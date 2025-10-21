cd90b07ac1e430bf5f06d2472a26af08b1bb441f
Vinukrishna05 <vinukrishna0508@gmail.com>
2025-10-21 15:26:20 +0530

    vk oct 21 3:20

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Home.css'
+import { Link } from 'react-router-dom';
 
 function Home() {
   const [opacity, setOpacity] = useState(1);
@@ -39,15 +40,61 @@ function Home() {
                 <h1>Ithis Medicity Empowering Future Leaders in <br /> Healthcare Through Excellence and Integrity</h1>
               </div>
             </section>
-            <section className='home_section_2'>
-              <div className="row">
+            <section className='home_section_2 '>
+              <div className="row justify-content-between px-3 py-4">
                 <div className="col-5">
-                  1
+                  <img src="/Homeimages/home_section_2_img.png" className='img-fluid' alt="" />
                 </div>
-                <div className="col-7">
+                <div className="col-6 d-flex flex-column justify-content-between">
                   <h4>Ithis Nursing College, established in 2025, is a leading institution dedicated to advancing nursing education and healthcare excellence in Kerala.
                     The college aims to nurture skilled, compassionate, and ethical nursing professionals who can meet the evolving demands of the healthcare industry
                   </h4>
+                  <div className="d-flex justify-content-start">
+                    <Link to="/about">
+                      <button className="apply-btn btn rounded-pill me-4">
+                        Know More
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className='home_section_3 '>
+              <h2 className='text-start mx-3'>Facilities</h2>
+              <div className='home_sec_3_container row mx-3'>
+                <div className="col d-flex flex-column align-items-center justify-content-evenly py-5">
+                  <img src="/public/Homeimages/library.png" className='' alt="" />
+                  <h3 className='py-4'>Library</h3>
+                  <h5 className='pb-4'>Ithis Medicityprovides D.Pharm and B.Sc Nursing programs, combining comprehensive theoretical knowledge </h5>
+                  <button className="apply-btn btn rounded-pill me-4">Explore More</button>
+                </div>
+                <div className="home_3_center_div col d-flex flex-column align-items-center justify-content-evenly py-5">
+                  <img src="/public/Homeimages/computer.png" className='' alt="" />
+                  <h3 className='py-4'>Computer Facilities</h3>
+                  <h5 className='pb-4'>Ithis Medicityprovides D.Pharm and B.Sc Nursing programs, combining comprehensive theoretical knowledge </h5>
+                  <button className="apply-btn btn rounded-pill me-4">Explore More</button>
+                </div>
+                <div className="col d-flex flex-column align-items-center justify-content-evenly py-5">
+                  <img src="/public/Homeimages/hostel.png" className='' alt="" />
+                  <h3 className='py-4'>Hostel</h3>
+                  <h5 className='pb-4'>Ithis Medicityprovides D.Pharm and B.Sc Nursing programs, combining comprehensive theoretical knowledge </h5>
+                  <button className="apply-btn btn rounded-pill me-4">Explore More</button>
+                </div>
+              </div>
+            </section>
+            <section className='home_section_4 '>
+              <div className="row py-5">
+                <div className="col">
+                  <img src="/Homeimages/ietcop-2048x896.png" alt="Logo" className="circular-img"/>
+                  <h5 className='py-3'>iet College of Pharmacy</h5>
+                </div>
+                <div className="col">
+                  <img src="/public/Homeimages/iet-institute-of-allied-health-science-logo.png" alt="Logo" className="circular-img"/>
+                  <h5 className='py-3'>iet College of Pharmacy</h5>
+                </div>
+                <div className="col">
+                  <img src="/public/Homeimages/ithis_hospital.png" alt="Logo" className="circular-img px-4"/>
+                  <h5 className='py-3'>iet College of Pharmacy</h5>
                 </div>
               </div>
             </section>
