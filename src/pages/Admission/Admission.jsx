e7e0e8e4668d9678290e2069b3d8a5852490b545
Vinukrishna05 <vinukrishna0508@gmail.com>
2025-10-24 09:44:23 +0530

    vk Oct 24 9:43

--- src/pages/Admission/Admission.jsx
+++ src/pages/Admission/Admission.jsx
@@ -37,48 +37,215 @@ function Admission() {
                 <div className="col form-column">
                     <div className="form-wrapper">
                         <form className="form-content">
-                            <label className="input-box" htmlFor="name">
-                                Name
-                                <input type="text" id="name" name="name" />
-                            </label>
-                            <label className="input-box" htmlFor="mobile">
-                                Mobile No
-                                <input type="number" id="mobile" name="mobile" />
-                            </label>
-                            <label className="input-box" htmlFor="email">
-                                Email
-                                <input type="email" id="email" name="email" />
-                            </label>
-                            <label className="input-box" htmlFor="age">
-                                Age
-                                <input type="number" id="age" name="age" />
-                            </label>
-                            <label className="input-box" htmlFor="age">
-                                Age
-                                <input type="number" id="age" name="age" />
-                            </label>
-                            <label className="input-box" htmlFor="age">
-                                Age
-                                <input type="number" id="age" name="age" />
-                            </label>
-                            <label className="input-box" htmlFor="age">
-                                Age
-                                <input type="number" id="age" name="age" />
-                            </label>
-                            <label className="input-box" htmlFor="age">
-                                Age
-                                <input type="number" id="age" name="age" />
-                            </label>
-                            <label className="input-box" htmlFor="age">
-                                Age
-                                <input type="number" id="age" name="age" />
-                            </label>
-                            <label className="input-box" htmlFor="age">
-                                Age
-                                <input type="number" id="age" name="age" />
-                            </label>
-                            {/* Add more fields */}
+                            {/* Row 1: Name & Mobile */}
+                            <div className="row-inputs">
+                                <label>
+                                    Name
+                                    <input type="text" name="name" />
+                                </label>
+                                <label>
+                                    Mobile No
+                                    <input type="number" name="mobile" />
+                                </label>
+                            </div>
+
+                            {/* Row 2: Email & DOB */}
+                            <div className="row-inputs">
+                                <label>
+                                    Email
+                                    <input type="email" name="email" />
+                                </label>
+                                <label>
+                                    DOB
+                                    <input type="date" name="dob" />
+                                </label>
+                            </div>
+
+                            {/* Row 3: Gender */}
+                            <div className="row-inputs gender-row">
+                                <label className="gender-label d-block text-center mb-2">Gender</label>
+                                <div className="gender-options d-flex justify-content-center gap-4">
+                                    <label className="d-flex align-items-center">
+                                        <input type="radio" name="gender" value="male" />
+                                        <span className="ms-1">Male</span>
+                                    </label>
+                                    <label className="d-flex align-items-center">
+                                        <input type="radio" name="gender" value="female" />
+                                        <span className="ms-1">Female</span>
+                                    </label>
+                                    <label className="d-flex align-items-center">
+                                        <input type="radio" name="gender" value="other" />
+                                        <span className="ms-1">Other</span>
+                                    </label>
+                                </div>
+                            </div>
+
+
+                            {/* Row 4: Address */}
+                            <div className="row-inputs">
+                                <label className="full-width">
+                                    Address
+                                    <input type="text" name="address" />
+                                </label>
+                            </div>
+
+
+
+                            {/* Row 7: Aadhar Number & Email */}
+                            <div className="row-inputs">
+                                <label>
+                                    Aadhar Number
+                                    <input type="number" name="aadhar" />
+                                </label>
+                                <label>
+                                    Email
+                                    <input type="email" name="email2" />
+                                </label>
+                            </div>
+
+                            {/* Row 8: Blood Group & Nationality */}
+                            <div className="row row-inputs align-items-start">
+                                <label className='col mx-0'>
+                                    Blood Group
+                                    <select name="bloodgroup">
+                                        <option value=""></option>
+                                        <option value="A+">A+</option>
+                                        <option value="B+">B+</option>
+                                        <option value="O+">O+</option>
+                                        <option value="AB+">AB+</option>
+                                    </select>
+                                </label>
+
+                                <div className="col nationality_options">
+                                    <label className="d-block mb-2">Nationality</label>
+                                    <div className='d-flex gap-4'>
+                                        <label className="d-flex align-items-center">
+                                            <input type="radio" name="nationality" value="Indian" />
+                                            <span className="ms-1">Indian</span>
+                                        </label>
+                                        <label className="d-flex align-items-center">
+                                            <input type="radio" name="nationality" value="Other" />
+                                            <span className="ms-1">Other</span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            {/* Row 9: Caste & Category */}
+                            <div className="row-inputs">
+                                <label>
+                                    Caste
+                                    <input type="text" name="caste" />
+                                </label>
+                                <label>
+                                    Category
+                                    <input type="text" name="category" />
+                                </label>
+                            </div>
+
+                            <h3>Educational Details</h3>
+
+                            {/* Row 5: Course & Qualification */}
+                            <div className="row-inputs">
+                                <label>
+                                    Course
+                                    <select name="course">
+                                        <option value="">Choose Prefered Course</option>
+                                        <option value="bsc">B.Sc Nursing</option>
+                                        <option value="dpharm">D.Pharm</option>
+                                    </select>
+                                </label>
+                                <label>
+                                    Qualification
+                                    <input type="text" name="qualification" />
+                                </label>
+                            </div>
+
+                            {/* Row 9: Institute & State of Last Qualification */}
+                            <div className="row-inputs">
+                                <label>
+                                    Institute of Last Qualification
+                                    <input type="text" name="lastInstitute" />
+                                </label>
+                                <label>
+                                    State of Qualification
+                                    <input type="text" name="qualificationState" />
+                                </label>
+                            </div>
+
+                            {/* Row 10: Year of Passing & Register Number */}
+                            <div className="row-inputs">
+                                <label>
+                                    Year of Passing
+                                    <input type="number" name="passingYear" />
+                                </label>
+                                <label>
+                                    Register Number
+                                    <input type="text" name="registerNumber" />
+                                </label>
+                            </div>
+
+                            {/* Row 11: Marks Section */}
+                            <div className="marks-section">
+                                <h4 className="marks-section-label">Marks Obtained</h4>
+
+                                <div className="marks-row">
+                                    <label className="marks-label">English</label>
+                                    <input type="number" name="englishMarks" className="marks-input" />
+                                </div>
+
+                                <div className="marks-row">
+                                    <label className="marks-label">Chemistry</label>
+                                    <input type="number" name="chemistryMarks" className="marks-input" />
+                                </div>
+
+                                <div className="marks-row">
+                                    <label className="marks-label">Physics</label>
+                                    <input type="number" name="physicsMarks" className="marks-input" />
+                                </div>
+
+                                <div className="marks-row">
+                                    <label className="marks-label">Biology / Equivalent</label>
+                                    <input type="number" name="biologyMarks" className="marks-input" />
+                                </div>
+
+                                <div className="marks-row">
+                                    <label className="marks-label">Total Marks</label>
+                                    <input type="number" name="totalMarks" className="marks-input" />
+                                </div>
+                            </div>
+
+                            <h3>Documents uploaded along with the application</h3>
+
+                            {/* Row 1: Passport Photo */}
+                            <div className="row-inputs">
+                                <label className="full-width">
+                                    Upload Passport Size Photo
+                                    <input type="file" name="photo" />
+                                </label>
+                            </div>
+
+                            {/* Row 2: SSLC / Equivalent Certificate */}
+                            <div className="row-inputs">
+                                <label className="full-width">
+                                    Copy of relevant page of SSLC / Equivalent Board Certificate as proof of Age and DOB (PDF)
+                                    <input type="file" name="ageProof" accept=".pdf" />
+                                </label>
+                            </div>
+
+                            {/* Row 3: Qualifying Exam Certificate / Marksheet */}
+                            <div className="row-inputs">
+                                <label className="full-width">
+                                    Copy of Certificate and Marklist of Qualifying Examination (Plus Two / CBSE / PDF of Result Sheet from Website)
+                                    <input type="file" name="qualifyingCert" accept=".pdf" />
+                                </label>
+                            </div>
+
+
                         </form>
+
+
 
                         {/* Fixed Submit Button */}
                         <div className="form-button">
